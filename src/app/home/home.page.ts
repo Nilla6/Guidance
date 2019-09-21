2fca2bb9393fa0fe074aaf0e24f5b9a4f1156126
FourEyedFreak <necolema@go.olemiss.edu>
2019-09-21 17:02:46 -0500

    Created page for map

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,16 +1,28 @@
 import { Component } from '@angular/core';
-
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
 
-  constructor() {
+  constructor(public navCtrl: NavController) {
     
-    
+  }
+
+  GoToClass(): void {
+    //this.navCtrl.navigateForward();  
+  }
+
+  GoToMap(): void{
+
+  }
+
+  GoToOffice():void {
+
   }
   
 }
